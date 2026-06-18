d15adb939ce3a81184e8b7ddd6dcdabda039a0b9
Sumire-sudo <catharinavanucchi@gmail.com>
2026-06-18 18:05:16 -0300

    exercicio 2 e 3

--- src/server.js
+++ src/server.js
@@ -5,27 +5,37 @@ const server = http.createServer((req, res) => {
     const url= req.url;
     const method = req.method;
 
-    if( url === "/GET"){
+    if( url === "/"){
         res.writeHead(200, {"Contet-Type" : "text/plain"});
+        console.log('GET! >.<');
         res.end("Get!");
-    }else if(url === "/sobre" && method === "GET"){
+    }else if(url === "/sobre" && method === 'GET'){
         res.writeHead(200, {"Contet-Type" : "text/plain"});
         res.end("Sobre!");
-    }else if(url === "/status"){
-        const statusDoServidor = {
-            status: 'UP',
-            mensagem: 'O servidor está ativo',
-            uptime: process.uptime(), 
-            timestamp: new Date().toISOString() 
-        };
+        console.log('Sobre!>//<');
+    }else if(url === "/status" && method === 'GET'){
+        res.end(JSON.stringify({mensagem: "Servidor funcionando^.^"}));
+        console.log("test");
+        }else if(url.startsWith("/aluno") && method === 'GET'){
+            const id = url.split("/")[2];
         
-        res.end(JSON.stringify(statusDoServidor));
-    }else {
-        req.end("página nâo encontrada");
+            const alunos = [{id:1, nome:"ana", turma:"2 ano",curso:"DS"},
+                {id:2, nome:"ana", turma:"2 ano",curso:"DS"},
+                {id:3, nome:"ana", turma:"2 ano",curso:"DS"},
+                {id:4, nome:"ana", turma:"2 ano",curso:"DS"},
+                {id:5, nome:"ana", turma:"2 ano",curso:"DS"}];
+           
+            //if(id){
+            //}
+            console.log(JSON.stringify(alunos));
+            res.end(JSON.stringify(alunos));
+        } 
+        else {
+        res.end("página nâo encontrada");
         res.statusCode = 404;
     }
-})
+});
 
 server.listen(3000,()=>{
-    console.log('rodando em localhos:3000');
-})
+    console.log('rodando em http://localhost:3000');
+});
